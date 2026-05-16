70305d22277298cc44367720d8bc02f53b01500e
GURUMITHRAN-15 <vgurumithran08@email.com>
2026-05-16 22:47:42 +0530

    first folder in 2nd branch

---
+++ py-branchesex/hello.py
@@ -0,0 +1,10 @@
+def welcome(name):
+    
+    a=int(input("choose an no"))
+    if a==1:
+        print("suprise motherfucker")
+    else :
+        print("you are a pussy")
+    return f"Hello, {name}!"
+
+welcome(input())
